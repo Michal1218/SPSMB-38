f5a103f27e6f70bb3642f1a873213b980a8a2aa7
Michal Bezděk <michal.bezdek@student.spsmb.cz>
2021-09-15 10:59:46 +0200

    added new lib

---
+++ src/MetodaUsage.java
@@ -0,0 +1,18 @@
+public class MetodaUsage {
+    public static void main(String[] args) {
+        int a = MatUtils.s(5);
+        System.out.println(a);
+        int v = MatUtils.v(5);
+        System.out.println(v);
+        int sum = MatUtils.sum(1, 2);
+        System.out.println(sum);
+        double sCyr = (int) MatUtils.sCyr(5);
+        System.out.println(sCyr);
+        int factorial MatUtils.factorial(25);
+        System.out.println(factorial);
+        boolean isEven MatUtils.isEven(3)
+        System.out.println(isEven);
+        boolean primeNumber MatUtils.primeNumber(2);
+        System.out.println(primeNumber);
+    }
+}
